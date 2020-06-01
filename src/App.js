0013578117ef7ec5bc39606ed3ee46b90ab1d3ea
Jefersson <jeferssonmunoz6@gmail.com>
2020-06-01 01:24:23 -0500

    Agregado components login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <>
+   <header>
+      <nav>
+       <ul>
+
+        <li> <a href="#">Contacto</a></li>
+
+          </ul>
+
+       </nav>
+
+
       </header>
-    </div>
-  );
+      <section>
+
+
+      <div class="tarjeta">
+      <header>
+          <h2>Ingreso</h2>
+
+          </header>
+          <form class="login">
+              <div>
+          <label>Correo</label>
+          <i class="fas fa-user" aria-hidden="true"></i>
+          <input type="email" placeholder="Ingrese correo"/>
+                  </div>
+              <div>
+          <label>Contraseña</label>
+              <i class="fas fa-lock" ></i>
+              <input type="password"placeholder="Ingresa contraseña"/>
+                  </div>
+              <div>
+          <label>Contraseña</label>
+              <i class="fas fa-lock" ></i>
+              <input type="password" placeholder="Ingrese contraseña" />
+                  </div>
+              <button class="boton">Ingresar</button>
+              <label>FECHA</label>
+
+          </form>
+             <div class ="link">
+         <p class="mensaje">Olvidaste tu contraseña?</p>   <a href="recuperar.html">Recuperar contraseña</a>
+
+
+                  </div>
+          <div class="link2">
+
+              <p class="mensaje2">No estas registrado?</p>   <a href="registro.html">Registrar</a>
+          </div>
+
+
+          </div>
+            </section>
+
+      </>
+
+
+
+  ) ;
 }
 
 export default App;
